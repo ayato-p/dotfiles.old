3ea117729cf3c5b8b19448fa25e12aaca58d5be1
ayato_p <ayato.skycrawlers@gmail.com>
2016-03-04 08:04:08 +0900

    [emacs] Add ace-jump keybindings :)

--- .emacs.d/modules/setup-key-bindings.el
+++ .emacs.d/modules/setup-key-bindings.el
@@ -10,6 +10,21 @@
              ("C-x g" . magit-status)
              ("C-c t" . toggle-truncate-lines)))
 
+(use-package ace-jump-mode
+  :config
+  (defun add-keys-to-ace-jump-mode (prefix c &optional mode)
+    (define-key global-map
+      (read-kbd-macro (concat prefix (string c)))
+      `(lambda ()
+         (interactive)
+         (funcall (if (eq ',mode 'word)
+                      #'ace-jump-word-mode
+                    #'ace-jump-char-mode) ,c))))
+
+  (loop for c from ?0 to ?9 do (add-keys-to-ace-jump-mode "H-" c))
+  (loop for c from ?a to ?z do (add-keys-to-ace-jump-mode "H-" c))
+  (loop for c from ?! to ?~ do (add-keys-to-ace-jump-mode "H-" c)))
+
 (use-package smartrep
   :config
   (progn
