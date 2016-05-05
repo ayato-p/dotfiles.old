362f72d84fc2c96501394ff20bfc197faa567b67
ayato_p <ayato.skycrawlers@gmail.com>
2016-05-05 14:10:47 +0900

    Add dired command

--- .emacs.d/modules/setup-basic.el
+++ .emacs.d/modules/setup-basic.el
@@ -58,6 +58,15 @@
     (recentf-mode 1)
     (setq recentf-max-menu-items 50)))
 
+(eval-after-load "dired"
+  '(progn
+     (define-key dired-mode-map (kbd "F") 'my-dired-find-marked-files)
+     (defun my-dired-find-marked-files (&optional arg)
+       "Open each of the marked files, or the file under the point, or when prefix arg, the next N files "
+       (interactive "P")
+       (let* ((fn-list (dired-get-marked-files nil arg)))
+         (mapc 'find-file fn-list)))))
+
 (use-package ace-isearch
   :config
   (global-ace-isearch-mode 1))
