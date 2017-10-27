a3f9222be269cac937a10e0d6bc44b9c24aa6000
ayato_p <ayato.skycrawlers@gmail.com>
2017-10-27 09:10:32 +0900

    Add ace-jump-mode

--- .emacs.d.new/init.el
+++ .emacs.d.new/init.el
@@ -257,6 +257,22 @@
   (setq mykie:use-major-mode-key-override t)
   (mykie:initialize))
 
+(use-package ace-jump-mode
+  :config
+  (defun add-keys-to-ace-jump-mode (prefix c &optional mode)
+    (define-key global-map
+      (read-kbd-macro (concat prefix (string c)))
+      `(lambda ()
+         (interactive)
+         (funcall (if (eq ',mode 'word)
+                      #'ace-jump-word-mode
+                    #'ace-jump-char-mode) ,c))))
+
+  (loop for c from ?0 to ?9 do (add-keys-to-ace-jump-mode "H-" c))
+  (loop for c from ?a to ?z do (add-keys-to-ace-jump-mode "H-" c))
+  (loop for c from ?0 to ?9 do (add-keys-to-ace-jump-mode "H-M-" c 'word))
+  (loop for c from ?a to ?z do (add-keys-to-ace-jump-mode "H-M-" c 'word)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;
 ;;;
 ;;; helm
